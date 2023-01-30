c17772f5fa77ffad9b0732ac578903693d89bd99
benzics <benjaminnicholas29@gmail.com>
2023-01-30 20:51:34 +0100

    added php unit

---
+++ src/Controller/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/api/users/register", name="user_register")
+     */
+    public function register(Request $request, ValidatorInterface $validator): JsonResponse
+    {
+        
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UserController.php',
+        ]);
+    }
+}
